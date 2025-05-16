240c16763023e39d9ca5ae4fbd8018133e8dea56
Serhii master <y7913092_w@iespoblenou.org>
2025-05-16 08:26:18 +0200

    Conection create

--- Biblioteca_System_Manager/src/infrastructure/Conexion.java
+++ Biblioteca_System_Manager/src/infrastructure/Conexion.java
@@ -1,4 +1,28 @@
 package infrastructure;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+    private static final String URL = "jdbc:mysql://mysqlscrum-iespoblenou-6495.d.aivencloud.com:26435/defaultdb?sslMode=REQUIRED";
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = "AVNS_Ico65QJgEn-rjTG7_Bq";
+
+    private Connection conn;
+
+    public Conexion() {
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión exitosa.");
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
 }
